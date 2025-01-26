517cfbc497eb258d49d637b1ef72af98236a96c5
is-xiaohu-sigh <1137247090@qq.com>
2025-01-26 19:04:45 +0800

    feat: add

--- db/index.js
+++ db/index.js
@@ -1,11 +1,12 @@
 // 导入mysql
 const mysql = require("mysql")
 // 创建数据库的连接
-const db =mysql.createPool({
+const db = mysql.createPool({
 	host:'localhost',
-	user:'back_system',
+	user:'root',
 	password:'123456',
-	database:'back_system'
+	database:'back_system',
+	port:3306
 })
 
 // 对外暴露数据库
